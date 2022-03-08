ed55200041387add4ac990cb1c2d55cc4b7fdcc7
HarveyH <521013@chrome.escg.ac.uk>
2022-03-08 15:22:09 +0000

    More work on character movement and mutli-input

---
+++ FMP/Assets/Red Light Green Light/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+
+public class PlayerInputHandler : MonoBehaviour
+{
+	
+	private PlayerInput playerInput;
+	private Mover mover;
+	private void Awake()
+	{
+		playerInput = GetComponent<PlayerInput>();
+		var movers = FindObjectOfType<Mover>();
+		var index = playerInput.playerIndex;
+		
+
+		
+	}
+
+	public void OnMove(CallbackContext context)
+	{
+		if (mover != null)
+
+			mover.SetInputVector(context.ReadValue<Vector2>());
+		
+	}
+	
+}
